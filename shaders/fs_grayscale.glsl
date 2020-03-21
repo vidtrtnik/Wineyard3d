a9db5511339ef6f657041d0929fad36a02e2b928
Vid Trtnik <vt4438@student.uni-lj.si>
2020-03-21 11:36:13 +0100

    Update shaders

--- shaders/fs_grayscale.glsl
+++ shaders/fs_grayscale.glsl
@@ -2,11 +2,14 @@ precision highp float;
 
 varying vec2 vVertexPosition;
 uniform sampler2D uTex;
+uniform float vx;
+uniform float vy;
+uniform float vz;
 
 void main()
 {
     vec2 texCoord = vec2( vVertexPosition.s, vVertexPosition.t ) * 0.5 + 0.5;
     vec4 color = texture2D(uTex, texCoord);
-    float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+    float gray = dot(color.rgb, vec3(vx, vy, vz));
     gl_FragColor = vec4(vec3(gray), 1.0);
 }
--- shaders/fs_texture.glsl
+++ shaders/fs_texture.glsl
@@ -1,10 +1,13 @@
+precision highp float;
+
 varying highp vec2 vTextureCoord;
 varying highp vec3 vLightWeight;
 
 uniform sampler2D uSampler;
+uniform float uAlpha;
 
 void main(void)
 {
   highp vec4 textureColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
-  gl_FragColor = vec4(textureColor.rgb * vLightWeight, textureColor.a);
+  gl_FragColor = vec4(textureColor.rgb * vLightWeight, uAlpha);
 }
